ad39be0bc4c46d1d0e3e1bdca48e496cc2afb815
Thomas Gozalie <114067350+regencode@users.noreply.github.com>
2022-11-30 17:01:00 +0700

    Add files via upload

--- 2. decipher.js
+++ 2. decipher.js
@@ -2,29 +2,21 @@
 
 function digitalDecipher(message, key){
     const alphabets = ['buffer','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-    let myFunc = num => Number(num);
-    var keyArr = Array.from(String(key), myFunc);
-    let output = [];
-    let word = [];
-    let i;
     let j = 0;
-
-    for(i = 0; i < message.length; i++){ //get output array of message after using key
-        if(j === keyArr.length){
+    var keyString = key.toString();
+    let output = [], word = []
+    
+    for(let i = 0; i < message.length; i++){
+        if(j === keyString.length){
             j = 0
         }
-            output[i] = message[i] - keyArr[j]
+            output[i] = message[i] - keyString[j]
+            word[i] = alphabets[output[i]];
             j++
     }
-
-    for(i = 0; i < output.length; i++){//get word from the output array
-        word[i] = alphabets[output[i]];
-    }
-    let txt = word.join("")
     
+    let txt = word.join("")
     console.log(txt);
-
 }
 
 digitalDecipher([20, 12, 18, 30, 21], 1939);
